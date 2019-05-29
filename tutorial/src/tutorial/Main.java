a8dacd9e24466ea8c2de7245f30951f27c9ef7de
Salsa-Dude <arias.joseph.a@gmail.com>
2019-05-28 21:15:14 -0400

    variables and datatypes

---
+++ tutorial/src/tutorial/Main.java
@@ -0,0 +1,32 @@
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Primitive DataTypes
+		// - not changeable
+		int hello_world = 5;
+		double num2 = 5.0;
+		boolean b = false;
+		// char uses single quotation marks
+		char c = 'i';
+		
+		// Non-Primitive DataTypes
+		// String uses double quotation marks
+		String str = "Tim29";
+		
+		int tim = hello_world;
+		
+		
+		
+		System.out.println(hello_world);
+		System.out.println(num2);
+		System.out.println(c);
+		System.out.println(b);
+		
+		System.out.println(tim);
+	}
+
+}
